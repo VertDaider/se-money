d93fbaf82f2e5a8f00c48a486c8f564262c3780a
VertDaider <serioussem92@gmail.com>
2022-02-23 14:38:16 +0700

    init commit

---
+++ se-money-portlet/src/main/java/ru/serioussem/se/money/constants/BudgetPortletKeys.java
@@ -0,0 +1,11 @@
+package ru.serioussem.se.money.constants;
+
+/**
+ * @author dovgopolov
+ */
+public class BudgetPortletKeys {
+
+	public static final String BUDGET =
+		"ru_serioussem_se_money_BudgetPortlet";
+
+}
